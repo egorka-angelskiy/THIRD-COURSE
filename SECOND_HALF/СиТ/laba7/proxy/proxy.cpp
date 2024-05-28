71259917a7744a8140298d6998c2b46bcab8633a
egorka_angelskiy <88832718+egorka-angelskiy@users.noreply.github.com>
2024-05-28 10:28:48 +0300

    Add files via upload

---
+++ SECOND_HALF/СиТ/laba7/proxy/proxy.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "socket_functions.h"
+
+int main() {
+
+	
+	cout << "Подключение!\n";
+	char msg[256];
+
+	Socket client_socket(msg);
+
+	int a;
+	std::cin >> a;
+	client_socket.SendPacketToClient("123");
+
+	return 0;
+}
---
+++ SECOND_HALF/СиТ/laba7/proxy/socket_functions.h
@@ -0,0 +1,54 @@
+#pragma once
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Socket
+{
+	SOCKET sock;
+public:
+	Socket(const char* ip)
+	{
+		WSAData wsaData;
+		WORD DLLVersion = MAKEWORD(2, 1);
+		if (WSAStartup(DLLVersion, &wsaData) != 0) {
+			std::cout << "Error" << std::endl;
+			exit(1);
+		}
+
+		SOCKADDR_IN addr;
+		int sizeofaddr = sizeof(addr);
+		addr.sin_addr.s_addr = inet_addr(ip);
+		addr.sin_port = htons(1111);
+		addr.sin_family = AF_INET;
+
+		SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+		bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+		listen(sListen, SOMAXCONN);
+
+		sock = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+	}
+
+	void SendPacketToClient(const string& to_send)
+	{
+		if (sock == 0) {
+			std::cout << "Error #2\n";
+		}
+		else {
+			cout << to_send << std::endl;
+			cout << to_send.c_str() << std::endl;
+			cout << to_send.size() << std::endl;
+			send(sock, to_send.c_str(), to_send.size() + 1, NULL);
+		}
+	}
+
+	~Socket()
+	{
+		closesocket(sock);
+	}
+};
